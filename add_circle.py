7ca46624bb896965f503e3e63b980d1745b0aacc
noahhinojos <nahinojo@uci.edu>
2022-08-09 09:03:03 -0700

    Cleanup.

--- add_circle.py
+++ add_circle.py
@@ -1,17 +1,23 @@
 """
-Adds a circle to sketch based on inputted coordinates. 
+Adds circle to target sketch.
+
+Inputs:
+ - x-coordinate of center
+ - y-coordinate of center
+ - radius of circle. 
 """
+
 print("Initializing Macro...\n")
 try:
     target_sketch
 except:
     raise NameError(
-        "Please define target sketch as 'target_sketch'" 
-        + "using FreeCAD's python console."
+        "Please define target sketch as 'target_sketch' using FreeCAD's"
+        + "python console. Then, re-execute this macro."
     )
 
 print("Please input the circles coordinates")
-print("in the form [x_coord, y_coord, radius]")
+print("in the form {x_coord, y_coord, radius}")
 print("separated by commas.\n")
 coords = input()
 coords = coords.replace(" ","")
@@ -21,6 +27,7 @@ print("x-coordinate: " + str(coords[0]))
 print("y-coordinate: " + str(coords[1]))
 print("radius: "+ str(coords[2]) + '\n')
 
+# Appending circle using FreeCAD's syntax. 
 target_sketch.addGeometry(
     Part.Circle(
         App.Vector(coords[0],coords[1],0),
@@ -28,5 +35,4 @@ target_sketch.addGeometry(
         coords[2]),
     False)
 App.ActiveDocument.recompute()
-
 print("Circle Generated!")
