aaad6ed4c5260b212ba45925a7a3890a14b6bd21
nahinojo <noah.hinojos@gmail.com>
2022-09-23 10:04:37 -0700

    Improved console communication.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import sys
-path = input("Input path to myfreecadlib: \n").replace(" ","'")
-sys.path.append(path)
-def f1():
-    return True
-f1()
+
+path_to_mfc = input(
+    "Please input the parent folder containing myfreecadlib: \n"
+).replace(" ","'")
+
+sys.path.append(path_to_mfc)
+print("Path successfully appended!")
+print("You may now import myfreecadlib using FreeCAD's Python console.")
