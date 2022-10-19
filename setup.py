de52e5eb0b8cda19e656e91939c59144485b7261
nahinojo <noah.hinojos@gmail.com>
2022-10-19 13:26:09 -0700

    Fully automated writing myfreecadlib to path.

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 import os
 import sys
+import json
 
-print("Exectuing Sandbox")
-with open(os.getcwd()+"/.FreeCAD/user.cfg", 'r') as f:
-    pass
-        
+mfl_data_path = os.getcwd()+"/.FreeCAD/myfreecadlib.json"
 
-sys.path.append(path_to_mfl)
+try:
+    with open(mfl_data_path, 'r') as file:
+        mfl_module_path = json.load(file)['path']
+except:
+    with open(mfl_data_path, 'w') as file:
+        mfl_module_path = input(
+            "Please input path to myfreecadlib's parent folder:"
+        )
+        json.dump(
+            {'path':mfl_module_path},
+            file
+        )
+finally:
+    sys.path.append(mfl_module_path)
+
+print(
+    "You may now import myfreecadlib using FreeCAD's Python console."
+)
